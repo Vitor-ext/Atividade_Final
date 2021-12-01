c5b5d2d63fd0a58cf4c76fe9c72cf304982841c4
Vitor-ext <83734913+Vitor-ext@users.noreply.github.com>
2021-12-01 20:37:35 -0300

    Add files via upload

---
+++ TesteWhile.java
@@ -0,0 +1,30 @@
+package caixaeletronico;
+
+
+public class TesteWhile {
+    /*
+    LOOP WHILE VARIVEL DE CONTADOR
+    */
+    
+    public void testeLoop1(){
+    int cont = 0;
+    while(cont <= 10){
+        System.out.println("VALOR DA VARIAVEL CONT:"+ cont);
+        //cont= cont +1;//cont+= 1;
+        cont++;
+        
+                
+    
+    
+            }      
+        System.out.println("VALOR FINAL DO CONT: " + cont);               
+      
+    
+    
+      }
+           
+    
+    }
+    
+    
+
